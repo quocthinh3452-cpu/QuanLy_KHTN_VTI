56907b18a2febd24b28cb30a031eb76a1245ee94
unknown <ghesaivay23@gmail.com>
2026-04-13 00:53:19 +0700

    update project task 1.2

---
+++ crmKHTN/src/test/java/org/example/crmkhtn/CrmKhtnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crmkhtn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmKhtnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
